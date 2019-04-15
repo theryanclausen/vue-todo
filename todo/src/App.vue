62ad72c2668da4b092fd4f66cb688aa476458fb5
theryanclausen <theryanclausen@gmail.com>
2019-04-14 19:22:27 -0700

    initial

---
+++ todo/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <div class="todo">
+    <Header/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import {less} from 'less-loader'
+
+export default {
+  name: 'app',
+  components: {
+    Header
+  }
+}
+</script>
+
+<style lang="less">
+*{
+  margin:0;
+  padding:0;
+  box-sizing: border-box;
+}
+#app {
+  width:100%;
+  background: linear-gradient(to bottom right,darkred, silver, darkred);
+  min-height: 100vh;
+  padding-top:15px;
+  .todo{ 
+    margin: 0 auto;
+    width:500px;
+    border-radius:24px;
+    border:5px solid darkblue;
+    background: #FEFFD1;
+  }
+}
+</style>
+
---
+++ todo/src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <h1>Here's what you gotta do!</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+};
+</script>
+
+<style lang="less" scoped>
+header {
+    width:100%;
+  h1 {
+    width: 100%;
+    text-align: center;
+  }
+}
+</style>
+
+
